5ed1d0784d5c4ec0034ebb8bf3ef2c7ea0ba7555
petcu1004 <kty4585@naver.com>
2023-02-07 16:25:28 +0900

    20230207

---
+++ BaekJoon/2차원 배열/2563.py
@@ -0,0 +1,23 @@
+ip=int(input())
+l = [[0 for j in range(100)] for i in range(100)]
+
+for i in range(100):
+    for j in range(100):
+        l[i][j]=0 #0으로 초기화
+
+for i in range(ip):
+    x, y=map(int, input().split())
+    for i in range(100):
+        for j in range(100):
+            if(i>=x and i<x+10) and (j>=y and j<y+10):
+                l[i][j]+=1
+k=0
+
+for i in range(100):
+    for j in range(100):
+        if(l[i][j]>1):
+            k+=l[i][j]-1
+
+m=ip*100
+
+print(m-k)
