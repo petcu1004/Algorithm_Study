ffb413b4699e2f1edd72736cac743b93d0b13f6e
petcu1004 <kty4585@naver.com>
2023-02-10 22:54:32 +0900

    20230210

---
+++ Programmers/완전탐색/0210.py
@@ -0,0 +1,29 @@
+def solution(sizes):
+
+    a=[]
+    b=[]
+    
+    for i in range(len(sizes)):
+        if sizes[i][0]<=sizes[i][1]:
+            res=sizes[i][0]
+            sizes[i][0]=sizes[i][1]
+            sizes[i][1]=res
+            
+        a.append(sizes[i][0])
+        b.append(sizes[i][1])
+    # print(sizes)
+    a.sort(reverse=1)
+    b.sort(reverse=1)
+    return a[0]*b[0]
+
+
+    #------------------------------#
+def solution(sizes):
+    row = 0
+    col = 0
+    for a, b in sizes:
+        if a < b:
+            a, b = b, a
+        row = max(row, a)
+        col = max(col, b)
+    return row * col
