1784086fcdff64df6f1c8bdbe5cd7cb6f5c3fd76
petcu1004 <kty4585@naver.com>
2023-02-21 22:53:42 +0900

    20230221 - SQL

--- BaekJoon/DFS,BFS/2606.py
+++ BaekJoon/DFS,BFS/2606.py
@@ -33,3 +33,35 @@ def bfs(i):
 
 root_node=1
 print(bfs(root_node))
+
+
+
+
+
+########################################
+
+
+from sys import stdin
+
+n = int(stdin.readline())
+v = int(stdin.readline())
+
+graph = [ [] for _ in range(n+1) ]
+visited = [0] * (n+1)
+
+for i in range(v) : 
+    a, b = map(int, stdin.readline().split())
+
+    graph[a] += [b]
+    graph[b] += [a]
+    print(graph)
+
+def dfs(k) : 
+    visited[k] = 1
+
+    for nx in graph[k] : 
+        if visited[nx] == 0 : 
+            dfs(nx)
+
+dfs(1)
+print(sum(visited)-1)
