323298e56c5aa7a6e53dd3148891e40a19d9d75f
petcu1004 <kty4585@naver.com>
2023-03-13 23:21:24 +0900

    20230313

---
+++ BaekJoon/반복문/11021.py
@@ -0,0 +1,17 @@
+## A+B - 7
+
+import sys
+
+input=sys.stdin.readline
+n=int(input())
+
+for i in range(n):
+    a, b= map(int, input().split())
+    # print("Case #", end='')
+    # print(i+1, end='')
+    # print(":", end=' ')
+    # print(a+b)
+
+    # print(f'Case #{i+1}: {a+b}')
+    
+    print("Case #", i+1, ": ", a+b, sep='')
---
+++ BaekJoon/반복문/15552.py
@@ -0,0 +1,10 @@
+## 빠른 A+B
+
+import sys
+
+input=sys.stdin.readline
+n=int(input())
+
+for i in range(n):
+    a, b= map(int, input().split())
+    print(a+b)
