eff9d9d4e577e9145e5cf5ba29c79782a21ea9b4
petcu1004 <kty4585@naver.com>
2023-03-17 00:39:44 +0900

    20230317 - 통과

--- Programmers/2021 Dev-Matching_BackEnd/03.16.py
+++ Programmers/2021 Dev-Matching_BackEnd/03.16.py
@@ -1,30 +1,43 @@
 ##로또의 최고 순위와 최저 순위
 
 def solution(lottos, win_nums):
-    answer = []
-    k=0
-    n=0
+    answer = [0, 0]
+    a, b, d =0, 0, 0
     for i in lottos:
-        # print(i)
         if i in win_nums:
-            # print("존재")
-            k+=1
+            a+=1
+        elif i==0:
+            b+=1
         else:
-            n+=1
-        if i==0:
-            k+=1
-        if i==0:
-            n+=1
-    
-    # if k==6:
-    #     answer.append(1)
-    # elif k==5:
-    #     answer.append(2)
-    # answer=[7-k, 7-]   
-    answer.append(7-k)
-    if n>5:
-        answer.append(6)
+            d+=1
+
+    corr=a+b
+    wrong = 6-(b+d)
+    c, w=0, 0
+    if corr==6:
+        answer[0]=1
+    elif corr==5:
+        answer[0]=2
+    elif corr==4:
+        answer[0]=3
+    elif corr==3:
+        answer[0]=4
+    elif corr==2:
+        answer[0]=5
     else:
-        answer.append(7-n)
+        answer[0]=6
+        
+    if wrong==6:
+        answer[1]=1
+    elif wrong==5:
+        answer[1]=2
+    elif wrong==4:
+        answer[1]=3
+    elif wrong==3:
+        answer[1]=4
+    elif wrong==2:
+        answer[1]=5
+    else:
+        answer[1]=6
     
     return answer
