b00e81553f29ba7bf216014bca842ebf303fc834
petcu1004 <kty4585@naver.com>
2023-03-23 01:13:23 +0900

    코드 참고 후 해결

--- Programmers/2021 Dev-Matching_BackEnd/169199.py
+++ Programmers/2021 Dev-Matching_BackEnd/169199.py
@@ -40,82 +40,89 @@ def solution(board):
             x=d[0]
             y=d[1]
             
-            for i in range(4):
-                
+            for i in range(4): 
+                nx, ny = x, y #처음 정해진 길로 쭈욱 미끄러지기 때문에 while문에서는 처음 큐에서 들어가는 값에서 시작해서 한 방향으로 가게
+
+                if board[nx][ny]=='G':
+                            print("찾음")
+                            # print(k)
+                            return v[x][y]
                 while(1):    
-                    nx=x+dx[i]
-                    ny=y+dy[i]
+                    nx=nx+dx[i] #nx = x+dx[i]이 안되는 이유) 한 방향으로 쭉 미끄러져야 하기 때문에 만약 
+                    ny=ny+dy[i]
                     print(dx[i], dy[i])
                     print(nx, ny)
 
-                    if board[nx-dx[i]][ny-dy[i]]=='G':
-                            print("찾음")
-                            print(k)
-                            return k
+                    # if board[nx-dx[i]][ny-dy[i]]=='G': #while문 안쪽에 못넣는 이유는 while에서는 일단 한칸씩 이동하는걸로 되는데 쭈욱 미끄러지다가 멈추는 곳에서 도착했는지를 봐야하는데 while문안에 있으면 그렇지 못하기 때문이다.
+                    #         print("찾음")
+                    #         print(k)
+                    #         return k
 
-                    if board[nx][ny]=='D' : #장애물을 만났을 때
-                        if board[nx-dx[i]][ny-dy[i]]=='G':
-                            print("찾음")
-                            print(k)
-                            return k
-                        print("장애물")
-                        q.append((nx-dx[i], ny-dy[i]))
-                        v[nx][ny]=1
-                        k+=1
-                        print(v)
-                        break
-                        
+  
                     if nx>=len(board) or nx<0 or ny>=len(board[0]) or ny<0:
                         print("나감")
-                        q.append((nx-dx[i], ny-dy[i]))
-                        k+=1
-                        print(v)
+                        nx-=dx[i]
+                        ny-=dy[i]
+                        # q.append((nx-dx[i], ny-dy[i]))
+                        # k+=1
+                        # print(v)
+                        break
+
+                    if board[nx][ny]=='D' : #장애물을 만났을 때
+                        print("장애물")
+                        nx-=dx[i]
+                        ny-=dy[i]
+                        # q.append((nx-dx[i], ny-dy[i]))
+                        # v[nx][ny]=1
+                        # k+=1
+                        # print(v)
                         break
                     
-                    if board[nx][ny]=='.' or board[nx][ny]=='R' or board[nx][ny]=='G' and v[nx][ny]==0: #빈공간일때
-                        print("빈공간")
-                        print(nx, ny)
+                    # if board[nx][ny]=='.' or board[nx][ny]=='R' or board[nx][ny]=='G' and v[nx][ny]==0: #빈공간일때
+                    #     print("빈공간")
+                    #     print(nx, ny)
 
-                        if board[nx+dx[i]][ny+dy[i]]=='D':
-                            print("다음 장애물 있음")
-                            v[nx][ny]=1
+                    #     if board[nx+dx[i]][ny+dy[i]]=='D':
+                    #         print("다음 장애물 있음")
+                    #         v[nx][ny]=1
                             
-                            q.append((nx, ny))
-                            break
-                            # if v[nx][ny]:
-                            #     break
+                    #         q.append((nx, ny))
+                    #         break
+                    #         # if v[nx][ny]:
+                    #         #     break
                             
                             
-                            print(v)
-                            continue
+                    #         print(v)
+                    #         continue
 
-                        v[nx][ny]=1
-                        x=nx
-                        y=ny
-                        continue
-                    else: #
-                        x=nx
-                        y=ny
-                        break
-                  
+                    #     v[nx][ny]=1
+                    #     x=nx
+                    #     y=ny
+                    #     continue
+                    # else: #
+                    #     x=nx
+                    #     y=ny
+                    #     break
+                if not v[nx][ny]: #쭉 미끄러지고 나서 그 지점을 체크해서 +1을 더해주는 것이다.
+                    # k+=1
+                    v[nx][ny]=v[x][y]+1
+                    q.append((nx, ny))
+        return -1
 
-
-                        
            
-                    
-                
-                    
-    bfs(a, b)                
+    result=bfs(a, b)
+    if result>0:
+         return result-1
+
+    return result               
             
         
-        
     
-    return -1
 
 
 
 k= ["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]
 
-for i in k:
-    print(i)
+# for i in k:
+#     print(i)
 print(solution(k))
