49ae89f210b8bc154ffce3c49189de2c7bf04e84
petcu1004 <kty4585@naver.com>
2023-03-24 02:50:17 +0900

    20230323

--- BaekJoon/트리, 그래프/11404.py
+++ BaekJoon/트리, 그래프/11404.py
@@ -7,26 +7,27 @@ n=int(input())
 m=int(input())
 res=[[inf]*n for _ in range(n)]
 
-for i in range(m):
-    a, b, c = map(int, input().split())
-    if res[a-1][b-1]>c:
-        res[a-1][b-1] = c
-    #res[a-1][b-1] = min(res[a-1][b-1], c)
-    
-# for i in range(n):
-#     res[i][i]=0
 
-for k in range(n): #k = 거쳐가는 노드
-    res[k][k]=0
-    for i in range(n): #i = 출발 노드
-        for j in range(n): #j = 도착 노드
-            # res[i][j]=min(res[i][j], res[i][k]+res[k][j])
-            if res[i][k]+res[k][j] < res[i][j]:
-                res[i][j] = res[i][k] + res[k][j]
+def floydWarshall():
+    for i in range(m):
+        a, b, c = map(int, input().split())
+        if res[a-1][b-1]>c:
+            res[a-1][b-1] = c
+        #res[a-1][b-1] = min(res[a-1][b-1], c)
 
-for i in range(n):
-    for j in range(n):
-        if res[i][j]==inf:
-            res[i][j]=0
-        print(res[i][j], end=' ')
-    print()
+    for k in range(n): #k = 거쳐가는 노드
+        res[k][k]=0 #자기 자신의 노드는 0으로 처리
+        for i in range(n): #i = 출발 노드
+            for j in range(n): #j = 도착 노드
+                # res[i][j]=min(res[i][j], res[i][k]+res[k][j])
+                if res[i][k]+res[k][j] < res[i][j]:
+                    res[i][j] = res[i][k] + res[k][j]
+
+    for i in range(n):
+        for j in range(n):
+            if res[i][j]==inf:
+                res[i][j]=0
+            print(res[i][j], end=' ')
+        print()
+
+floydWarshall()
