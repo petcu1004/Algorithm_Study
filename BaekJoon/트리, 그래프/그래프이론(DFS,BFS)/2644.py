41fdbf6d7c961391305154b9bee39cbe2adede07
petcu1004 <kty4585@naver.com>
2023-03-30 00:44:43 +0900

    히든 테이스 케이스 통과 X

--- BaekJoon/트리, 그래프/그래프이론(DFS,BFS)/2644.py
+++ BaekJoon/트리, 그래프/그래프이론(DFS,BFS)/2644.py
@@ -1,16 +1,36 @@
 ## 촌수계산
 from collections import deque
 
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited[v]=1
+
+    while q:
+        node=q.popleft()
+        for i in range(1,n):
+            if matrix[node][i]==1 and not visited[i]:
+                q.append(i)
+                res[i]=res[node]+1
+                visited[i]=1
+                # print(visited)/
+
+
 n=int(input())
 x, y = map(int, input().split())
 
-martix=[[0]*(n+1) for _ in range(n+1)]
+matrix=[[0]*(n+1) for _ in range(n+1)]
 visited=[0]*(n+1)
+res=[0]*(n+1)
 
 k=int(input())
 for i in range(k):
     a, b = map(int, input().split())
-    martix[a][b] = martix[b][a] = 1
+    matrix[a][b] = matrix[b][a] = 1
 
-def bfs(v):
-    
+               
+bfs(x)
+if res[y]>0:
+    print(res[y])
+else:
+    print(-1)
