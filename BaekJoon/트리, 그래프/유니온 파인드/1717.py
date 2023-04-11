b121e35b61baa86f2694c70cc2b149e7e1d515a7
petcu1004 <kty4585@naver.com>
2023-04-12 01:50:23 +0900

    유니온 파인드 복습

--- BaekJoon/트리, 그래프/유니온 파인드/1717.py
+++ BaekJoon/트리, 그래프/유니온 파인드/1717.py
@@ -1,16 +1,46 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+# import sys
+# sys.setrecursionlimit(10**6)
+# input = sys.stdin.readline
+
+# def find(x):
+#     if parent[x] != x:
+#         parent[x]= find(parent[x])
+#     return parent[x]
+
+# def union(a, b):
+#     a=find(a)
+#     b=find(b)
+#     #값이 더 작은 것이 부모 노드로 취급함.
+#     if a<b:
+#         parent[b]=a
+#     else:
+#         parent[a]=b
+
+# v, e = map(int, input().split())
+
+# parent = [i for i in range(v+1)]
+
+
+# for _ in range(e):
+#     k, a, b = map(int, input().split())
+#     if k==0:
+#         union(a, b)
+#     elif k == 1:
+#         if find(a)==find(b):
+#             print("YES")
+#         else:
+#             print("NO")
+
+
 
 def find(x):
-    if parent[x] != x:
-        parent[x]= find(parent[x])
+    if parent[x] !=x:
+        parent[x] = find(parent[x])
     return parent[x]
 
 def union(a, b):
     a=find(a)
     b=find(b)
-    #값이 더 작은 것이 부모 노드로 취급함.
     if a<b:
         parent[b]=a
     else:
@@ -18,14 +48,13 @@ def union(a, b):
 
 v, e = map(int, input().split())
 
-parent = [i for i in range(v+1)]
-
+parent=[i for i in range(v+1)]
 
 for _ in range(e):
     k, a, b = map(int, input().split())
     if k==0:
         union(a, b)
-    elif k == 1:
+    elif k==1:
         if find(a)==find(b):
             print("YES")
         else:
