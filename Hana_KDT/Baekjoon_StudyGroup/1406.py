a003e79fa0c5dfbd059ac41ed1033036e4b3f120
petcu1004 <kty4585@naver.com>
2023-05-25 22:47:25 +0900

    에디터 문제 해결

--- Hana_KDT/Baekjoon_StudyGroup/1406.py
+++ Hana_KDT/Baekjoon_StudyGroup/1406.py
@@ -1,36 +1,35 @@
-# 에디터
+# 에디터 - 성공
 import sys
 
 input = sys.stdin.readline
 
-n = list(input())
+l_stack = list(input().strip())
 
+r_stack = list()
 k = int(input())
 
-
-cursor = len(n)
 for i in range(k):
     a = input()
     if a[0] == "P":
-        n.insert(cursor, a[2])
-        cursor += 1
+        l_stack.append(a[2])
+        # cursor += 1
     elif a[0] == "L":
-        if cursor == 0:
+        if len(l_stack)==0:
             continue
         else:
-            cursor -= 1
+            r_stack.append(l_stack.pop(-1))
     elif a[0] == "D":
-        if cursor == len(n):
+        if len(r_stack)==0:
             continue
         else:
-            cursor += 1
-    elif a[0] == "B":
-        if cursor == 0:
+            l_stack.append(r_stack.pop(-1))
+    elif a[0] == "B":   
+        if len(l_stack)==0:
             continue
         else:
-            del n[cursor - 1]
-            cursor -= 1
+            l_stack.pop(-1)
 
-    if cursor < 0:
-        cursor = 0
-print("".join(n))
+for i in reversed(range(len(r_stack))):
+    l_stack.append(r_stack[i])
+
+print("".join(l_stack))
