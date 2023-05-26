893e36d9d9107f731abc734fa3641d4071be8b82
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-05-26 09:39:53 +0900

    덱 문제 해결

--- Hana_KDT/Baekjoon_StudyGroup/10866.py
+++ Hana_KDT/Baekjoon_StudyGroup/10866.py
@@ -1,42 +1,43 @@
 # 덱
 import sys
-input=sys.stdin.readline
 
-n=int(input())
+input = sys.stdin.readline
 
-deque=list()
+n = int(input())
+
+deque = list()
 for i in range(n):
-    a=input().split()
+    a = input().split()
 
-    if a[0]=="push_front":
+    if a[0] == "push_front":
         deque.insert(0, a[1])
-    elif a[0]=="push_back":
-        deque.insert(-1, a[1])
-    elif a[0]=="pop_front":
-        if len(deque)==0:
+    elif a[0] == "push_back":
+        deque.append(a[1])
+    elif a[0] == "pop_front":
+        if len(deque) == 0:
             print(-1)
         else:
             print(deque.pop(0))
-    elif a[0]=="pop_back":
-        if len(deque)==0:
+    elif a[0] == "pop_back":
+        if len(deque) == 0:
             print(-1)
         else:
             print(deque.pop(-1))
-    elif a[0]=="size":
+    elif a[0] == "size":
         print(len(deque))
-    elif a[0]=="empty":
-        if len(deque)==0:
+    elif a[0] == "empty":
+        if len(deque) == 0:
             print(1)
         else:
             print(0)
-    elif a[0]=="front":
-        if len(deque)==0:
+    elif a[0] == "front":
+        if len(deque) == 0:
             print(-1)
         else:
             print(deque[0])
-    elif a[0]=="back":
-        if len(deque)==0:
+    elif a[0] == "back":
+        if len(deque) == 0:
             print(-1)
         else:
             print(deque[-1])
-        
+    # print(deque)
