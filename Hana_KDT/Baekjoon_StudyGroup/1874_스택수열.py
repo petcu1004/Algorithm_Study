11160b6bc08de2d3f47384e2cc0f9fc46d73d354
petcu1004 <kty4585@naver.com>
2023-06-13 20:22:27 +0900

    06.13 Solved

---
+++ Hana_KDT/Baekjoon_StudyGroup/1874_스택수열.py
@@ -0,0 +1,28 @@
+n=int(input())
+
+stack=list()
+count=0
+# stack.append(0)
+for i in range(n):
+    a=int(input())
+    if(len(stack)==0):
+        for i in range(a):
+            count+=1
+            stack.append(count)
+            print('+')
+    else:
+        while(1):
+            if a<stack[-1]:
+                print('-')
+                stack.pop()
+                
+            elif a>stack[-1]:
+                count+=1
+                stack.append(count)
+                print('+')
+            else:
+                print('-')
+                stack.pop()
+            
+        
+    print(stack)
