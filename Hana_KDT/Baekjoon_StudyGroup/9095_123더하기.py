9e03f5afdbc81bc0848aa564eca7c9d8bf6b8b9a
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-06-13 14:36:00 +0900

    dp[3]에 값을 넣기 위해서 dp배열을 추가할 때 (n+3)으로 줘야했던 것임

--- Hana_KDT/Baekjoon_StudyGroup/9095_123더하기.py
+++ Hana_KDT/Baekjoon_StudyGroup/9095_123더하기.py
@@ -1,24 +1,25 @@
 import sys
 
-input=sys.stdin.readline
-ip= int(input())
+input = sys.stdin.readline
+ip = int(input())
+
+
 def add(n):
-
-    dp=[0]*12 #이 부분이 중요한 듯.. 이렇게 하지 않고 [0]*(n+1)을 하면 런타임 에러가 남
+    dp = [0] * (n + 3)  # 이 부분이 중요한 듯.. 이렇게 하지 않고 [0]*(n+1)을 하면 런타임 에러가 남
     # 기본값
-    dp[1]=1
-    dp[2]=2
-    dp[3]=4
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
 
-    if n<4:
+    if n < 4:
         return dp[n]
-    
-    for i in range(4, n+1):
-        dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
-        
+
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
     return dp[n]
 
 
 for i in range(ip):
-    k=int(input())
+    k = int(input())
     print(add(k))
